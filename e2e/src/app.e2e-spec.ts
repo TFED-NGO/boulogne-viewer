19d778e5e92883d1f63685758c5aaa1aa2398370
Jackson Meade <meade24j@email.unc.edu>
2024-02-28 20:53:28 -0500

    Rename to boulogne

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('alcuin-viewer app is running!');
+    expect(page.getTitleText()).toEqual('boulogne-viewer app is running!');
   });
 
   afterEach(async () => {
